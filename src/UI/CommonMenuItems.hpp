5573443f89f61b2faa297fd4839c84d52b16776d
Chronos Ouroboros <doom2.fan@gmail.com>
2024-03-25 11:08:55 -0300

    Add a confirmation prompt menu item.

---
+++ src/UI/CommonMenuItems.hpp
@@ -0,0 +1,56 @@
+/*
+ *  OuroborosModules
+ *  Copyright (C) 2024 Chronos "phantombeta" Ouroboros
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "../PluginDef.hpp"
+
+namespace OuroborosModules {
+namespace UI {
+    struct SafeMenuItem : rack::ui::MenuItem {
+      private:
+        static constexpr const char* defaultConfirmText = "Are you sure?";
+
+        std::string confirmText;
+        std::string confirmButtonText;
+        std::function<void ()> action;
+        bool alwaysConsume;
+
+      public:
+        SafeMenuItem (
+            std::string text,
+            std::string confirmButtonText,
+            std::function<void ()> action,
+            bool alwaysConsume = false,
+            std::string confirmText = defaultConfirmText
+        ) : confirmText (confirmText),
+            confirmButtonText (confirmButtonText),
+            action (action),
+            alwaysConsume (alwaysConsume) { this->text = text; }
+
+        SafeMenuItem (
+            std::string text,
+            std::function<void ()> action,
+            bool alwaysConsume = false,
+            std::string confirmText = defaultConfirmText
+        ) : SafeMenuItem (text, text, action, alwaysConsume, confirmText) { }
+
+        void onAction (const rack::event::Action& e) override;
+    };
+}
+}
